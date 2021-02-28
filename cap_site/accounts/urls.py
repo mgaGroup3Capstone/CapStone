f089e09e9b4898f870824e976a8952bbc254e991
Tyler Cleveland <46286834+Tyler-C2@users.noreply.github.com>
2021-02-27 23:12:29 -0500

    Update urls.py
    added login and register page. added name variables to paths

--- cap_site/accounts/urls.py
+++ cap_site/accounts/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home),
-    path('forum/', views.forum),
-    path('calendar/', views.calendar),
+    path('', views.home, name = "home"),
+    path('forum/', views.forum, name = "forum"),
+    path('calendar/', views.calendar, name = "calendar"),
+    path('login/', views.loginPage, name = "login"),
+    path('logout/', views.logoutUser, name = "logout"),
+    path('register/', views.registerPage, name = "register"),
 ]
